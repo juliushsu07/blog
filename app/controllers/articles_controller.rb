55a3238531c217dd503ce6cad32cfb7f32296d97
Julius Hsu <juliushsu07@gmail.com>
2017-12-08 17:59:09 +0000

    added CRUD for the blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,32 +1,33 @@
+
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
 
-
   def show
     @article = Article.find(params[:id])
   end
-
 
   def new
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def create
     @article = Article.new(article_params)
 
-   if @article.save
+    if @article.save
       redirect_to @article
     else
       render 'new'
     end
   end
 
-
   def update
-  @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
 
     if @article.update(article_params)
       redirect_to @article
@@ -35,6 +36,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
 
   private
     def article_params
